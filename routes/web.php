944962c170612ba8a6420d494352076db0cfca32
Haruka0403 <koharu02170403@gmail.com>
2020-05-30 03:12:59 +0000

    laravel 12

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,18 @@ Route::get('/', function () {
   return view('welcome');
 });
 
-//php9 テキスト
+//php9 テキスト php12テキスト（middleware)
 Route::group(['prefix' => 'admin'], function() {
-  Route::get('news/create', 'Admin\NewsController@add');
+  Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
-//php9課題
+//php9課題 php12課題（middleware)
 Route::group(['prefix' => 'admin/profile'], function(){
-  Route::get('create', 'Admin\ProfileController@add');
-  Route::get('edit' , 'Admin\ProfileController@edit');
+  Route::get('create', 'Admin\ProfileController@add')->middleware('auth');
+  Route::get('edit' , 'Admin\ProfileController@edit')->middleware('auth');
 });
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
